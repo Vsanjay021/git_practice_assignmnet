638437da2eb50ebf6c414c30d3e220f8b258babf
Anshika srivastava <112770564+anshika11srivastav@users.noreply.github.com>
2022-09-13 21:08:33 +0530

    Prime logic

--- prime.js
+++ prime.js
@@ -1,2 +1,11 @@
-let a=[1,2,3,4];
-console.log("yes");
+let num=13;
+let count=0;
+for(let i=0; i<=num; i++){
+if(num%i==0){
+count++;
+}
+if(count==2)
+console.log("Prime");
+else{
+console.log("Not Prime");
+}
